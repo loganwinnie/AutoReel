cfa349858198f6c18715495be778b72443f64071
Logan Winnie <loganwinnier@gmail.com>
2025-01-07 09:06:08 -0500

    Save routes

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,20 @@
 from typing import Annotated
 from fastapi import FastAPI, Query
-from reddit_posts import get_posts_from_subreddit, save_post_to_db
+from fastapi.concurrency import asynccontextmanager
+from reddit_posts import get_posts_from_subreddit, save_post_to_db, get_saved_posts_from_db, delete_saved_post_from_db
+from util import create_db_and_tables
 
-tags_metadata = [{"name": "Posts", "description": "Routes for handling reddit posts"}]
+tags_metadata = [{"name": "Posts", "description": "Routes for handling reddit posts."},
+                 {"name": "Save", "description": "Routes for saving posts."}]
 
-app = FastAPI(openapi_tags=tags_metadata)
 
-
-@app.get("/")
-def root():
-    return {"Hello": "World"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the Database
+    create_db_and_tables()
+    yield
+    
+app = FastAPI(openapi_tags=tags_metadata, lifespan=lifespan)
 
 
 @app.get("/posts", tags=["Posts"])
@@ -31,10 +36,32 @@ def get_posts(
     return {"posts": posts}
 
 
-@app.post("/posts", tags=["Posts"])
+@app.post("/save", tags=["Save"])
 def save_post(post_id: str):
     post = save_post_to_db(post_id=post_id)
     return {"post": post}
 
 
-# @app.post("/posts/save",)
+@app.get("/save", tags=["Save"])
+def get_saved_posts(
+    limit: Annotated[
+        int,
+        Query(
+            title="amount of posts to retrieve",
+        ),
+    ] = 15,
+    offset: Annotated[
+    int,
+    Query(
+        title="amount of posts to retrieve",
+    ),
+    ] = 0,
+):
+    posts = get_saved_posts_from_db(offset=offset,limit=limit)
+    return {"posts": posts}
+
+@app.delete("/save", tags=["Save"])
+def delete_saved_post(post_id:str):
+    post = delete_saved_post_from_db(post_id=post_id)
+    return {"deleted": post}
+
--- backend/reddit_posts.py
+++ backend/reddit_posts.py
@@ -1,8 +1,8 @@
 import praw
 import json
 from fastapi import HTTPException
-from prawcore.exceptions import NotFound
-
+from prawcore.exceptions import NotFound, ResponseException
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from config import (
     REDDIT_CLIENT_ID,
     REDDIT_CLIENT_SECRET,
@@ -12,7 +12,7 @@ from config import (
     engine,
 )
 
-from sqlmodel import Session
+from sqlmodel import Session, select
 from models import Post
 
 client_id = REDDIT_CLIENT_ID
@@ -30,27 +30,33 @@ reddit = praw.Reddit(
 
 
 def get_posts_from_subreddit(subreddit: str = "Autoreel", limit: int = 15):
+    try: 
+        custom_feed = reddit.multireddit(REDDIT_USERNAME, subreddit)
 
-    custom_feed = reddit.multireddit(REDDIT_USERNAME, subreddit)
+        post_data = []
 
-    post_data = []
+        for post in custom_feed.hot(limit=limit):
 
-    for post in custom_feed.hot(limit=limit):
+            post_dict = {
+                "title": post.title.strip(),
+                "url": post.url,
+                "content": post.selftext.strip(),
+                "subreddit": post.subreddit.display_name.strip(),
+                "post_id": post.id,
+            }
 
-        post_dict = {
-            "title": post.title.strip(),
-            "url": post.url,
-            "content": post.selftext.strip(),
-            "subreddit": post.subreddit.display_name.strip(),
-            "post_id": post.id,
-        }
+            post_data.append(post_dict)
 
-        print("SUBREDDIT", dir(post.subreddit))
-        post_data.append(post_dict)
+        jsonified_posts = json.dumps(post_data)
 
-    jsonified_posts = json.dumps(post_data)
+        return jsonified_posts
+    except ResponseException:
+        raise HTTPException(401, detail="Failed to authenticate Reddit Account.")
+    except NotFound:
+        raise HTTPException(404, detail="Subreddit not found.")
+    except:
+        raise HTTPException(500, detail="Failed to fetch post.")
 
-    return jsonified_posts
 
 
 def save_post_to_db(post_id: str):
@@ -59,23 +65,49 @@ def save_post_to_db(post_id: str):
 
     try:
         post = reddit.submission(id=post_id)
-        print("POST", post)
     except NotFound:
         raise HTTPException(404, detail="Post not found.")
     except:
         raise HTTPException(500, detail="Problem retrieving post.")
 
     with Session(engine) as session:
-        # try:
-        post_model = Post(
-            id=post.id,
-            title=post.title.strip(),
-            url=post.url.strip(),
-            content=post.selftext.strip(),
-            subreddit=post.subreddit.display_name.strip(),
-        )
-        session.add(post_model)
-        session.commit()
-        return post_model
-    # except:
-    #     raise HTTPException(500, detail="Unable to save post.")
+        try:
+            post_model = Post(
+                id=post.id,
+                title=post.title.strip(),
+                url=post.url.strip(),
+                content=post.selftext.strip(),
+                subreddit=post.subreddit.display_name.strip(),
+            )
+            post_json = post_model.model_dump_json()
+            session.add(post_model)
+            session.commit()
+            return post_json
+        except IntegrityError:
+            raise HTTPException(400, detail="Cannot save duplicate post.")
+        except:
+            raise HTTPException(500, "Failed to save post.")
+
+def get_saved_posts_from_db(offset:int = 0, limit:int = 15):
+    with Session(engine) as session:
+        try:
+           statement = select(Post).offset(offset=offset).limit(limit=limit)
+           posts = session.exec(statement=statement).all()
+           return posts
+        except:
+            raise HTTPException(500, "Failed to get posts.")
+
+
+def delete_saved_post_from_db(post_id: str):
+    with Session(engine) as session:
+        try:
+            statement = select(Post).where(Post.id == post_id)
+            results = session.exec(statement)
+            post = results.one()
+            session.delete(post)
+            session.commit()
+            return post.id
+        except NoResultFound:
+            raise HTTPException(404, "Post not found.")
+        except:
+            raise HTTPException(500, "Failed to delete post.")
